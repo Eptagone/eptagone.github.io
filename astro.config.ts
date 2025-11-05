edb13a870439bb3e9708b96773e39536d6957e53
Quetzal Rivera <quetzaldeveloper@outlook.com>
2025-11-04 21:08:10 -0600

    fix: remove file extension from canonical url chore: bump dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -7,9 +7,6 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
     site: "https://eptagone.dev",
     trailingSlash: "never",
-    build: {
-        format: "file",
-    },
     integrations: [
         sitemap({
             filter: url => !url.match(/https:\/\/eptagone\.dev\/tags/),
